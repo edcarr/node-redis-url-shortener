ece3a9fbb74c27109f656bd50507042bf90b069d
ed <eddiepcarr@gmail.com>
2017-01-13 22:05:58 +0000

    added POST and GET methods for shortening and expanding urls/hashes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,44 @@
 import { Router } from "express";
+import client from "../db/redis";
+import encode from "../lib/encode";
 
 const router = Router();
 
+router.get("/:hash", (req, res) => {
+    const { hash } = req.params;
+    if (hash) {
+        client.get(hash, (err, url) => {
+            if (!err && url) {
+                return res.redirect(url);
+            } else {
+                return res.status(400).send({message: "No url found for hash"});
+            }
+        });
+    } else {
+        return res.status(400).send({message: "No hash provided"});
+    }
+});
+
 router.post("/", (req, res) => {
-    
+    const { url } = req.body;
+    if (url) {
+        client.dbsize((err, count) => {
+            if (!err) {
+                const hash = encode(count);
+                client.set(hash, url, (err, result) => {
+                    if (!err) {
+                        res.status(200).send(result);
+                    } else {
+                        res.status(500).send({message: err})
+                    }
+                });
+            } else {
+                res.status(500).send({message: err});
+            }
+        });
+    } else {
+        res.status(400).send({message: "No url provided"});
+    }
 });
 
 export default router;
